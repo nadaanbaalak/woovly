4e0b4142b1274c2aae9d5427b3855acc1abd654c
nadaanbaalak <abhi.sharma2800015@gmail.com>
2020-06-26 17:47:37 +0530

    Added a generic Navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      <nav className="navbar navbar-dark bg-dark">
+        <a href="/" className="navbar-brand">
+          Woovly
+        </a>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
